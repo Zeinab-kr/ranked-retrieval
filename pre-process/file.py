955a308cfd625e2e13c39aa3642cd8bd59970b55
Zeinab <zeinab_kr@aut.ac.ir>
2024-01-24 01:13:23 +0330

    file opener initialization

---
+++ pre-process/file.py
@@ -0,0 +1,20 @@
+import json
+
+
+def open_json(path):
+    file = open(path, 'r')
+    data = json.load(file)
+    file.close()
+    return data
+
+
+def read_file(path):
+    file = open(path, 'r')
+    data = file.read()
+    file.close()
+    return data
+
+
+def write_file(path, data):
+    file = open(path, 'w', encoding='utf-8')
+    file.write(data)
