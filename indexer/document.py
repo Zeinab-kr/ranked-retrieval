005801139697d88abcd6d2f1bd195191d0e3a2ef
Zeinab <zeinab_kr@aut.ac.ir>
2024-01-25 16:36:37 +0330

    Document class

--- indexer/document.py
+++ indexer/document.py
@@ -1,9 +1,9 @@
 # miyad ye doc tarif mikone, vazne kalame too doco migire
 # ye araye ham dare ke position-haye kalame too doc toosh save mishe
 class Document:
-    def __init__(self, weight):
+    def __init__(self, weight, positions):
         self.weight = weight
-        self.positions = []
+        self.positions = positions
 
     def add_posting(self, pos):
         self.positions.append(pos)
@@ -11,3 +11,5 @@ class Document:
     def set_weight(self, weight):
         self.weight = weight
 
+    def get_weight(self):
+        return self.weight
