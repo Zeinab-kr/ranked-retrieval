3a6ac5bb7eb718681bd186dc9f0ced58993c4f32
Zeinab <zeinab_kr@aut.ac.ir>
2024-01-25 16:36:59 +0330

    Token class

--- indexer/token.py
+++ indexer/token.py
@@ -4,13 +4,22 @@ from document import *
 # ye token ya term ya kalame tarif mikone, tedad kalame too kole doc-H ro migire save mikone
 # ye list as doc-hayee ke kalame ro daran dare
 class Token:
-    def __init__(self, tf):
+    def __init__(self, token, tf, docs):
+        self.token = str(token)
         self.tf = tf
-        self.docs = []
+        self.docs = docs
 
-    def add_doc(self, weight):
-        self.docs.append(Document(weight))
+    def add_doc(self, doc):
+        self.docs.append(doc)
 
-    def add_posting(self, doc_index, posting):
-        self.docs[doc_index].add_posting(posting)
+    def set_tf(self, tf):
+        self.tf = tf
 
+    def get_tf(self):
+        return self.tf
+
+    def get_token(self):
+        return self.token
+
+    def get_weight_in_doc(self, doc_index):
+        return self.docs[doc_index].get_weight()
