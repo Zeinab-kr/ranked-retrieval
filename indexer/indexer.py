42537f827728a103f2a05a5f24c2d8a4ae53e342
Zeinab <zeinab_kr@aut.ac.ir>
2024-01-26 15:09:56 +0330

    indexing function modified

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -1,17 +1,15 @@
 import bisect
 
 from preprocess import file
-import token_class
-import document_class
+from token_class import Token
 import os
 
 data = file.open_json("../data/tokens.json")
-token = token_class.Token
-document = document_class.Document
 
 
 def make_index():
     tokens = []
+    tokens.extend([Token])
     print("reading files...")
     if os.path.exists("../data/indexes.json"):
         tokens = file.open_json("../data/indexes.json")
@@ -23,32 +21,34 @@ def make_index():
     print(data[0][1])
 
     counter = 0
-    tokens.append(token(words[0][0], words[0][1]))
-    tokens[0].extend_array(data[0][1])
+    tokens[0] = Token(words[0][0], words[0][1])
     tokens[0].add_doc(data[0][1])
     tokens[0].set_weight(data[0][1], 1)
     tokens[0].add_posting_to_doc(data[0][1], data[0][2])
 
     # data: (word, doc_id, posting)   words: (word, tf)
     for i in range(1, len(data)):
-        if data[i][0] == data[i-1][0]:  # if words are the same
-            if data[i][1] == data[i-1][1]:  # if focs are the same
-                tokens[i-1].increment_weight(data[i][1])
-            else:  # if docs aren't the same
-                tokens[i-1].extend_array(data[i][1])
-                tokens[i-1].add_doc(data[i][1])
-                tokens[i-1].set_weight(data[i][1], 1)
+        if i % 10000 == 0:
+            print("making index part {}".format(i/10000))
 
-            tokens[i - 1].add_posting_to_doc(data[i][1], data[i][2])
+        word = data[i][0]
+        doc_id = data[i][1]
+        posting = data[i][2]
+        if word == data[i-1][0]:  # if words are the same
+            if doc_id == data[i-1][1]:  # if focs are the same
+                tokens[counter].increment_weight(doc_id)
+            else:  # if docs aren't the same
+                tokens[counter].add_doc(doc_id)
+                tokens[counter].set_weight(doc_id, 1)
+
+            tokens[counter].add_posting_to_doc(doc_id, posting)
         else:  # if words aren't the same
             counter += 1
-            tokens.append(token(words[counter][0], words[counter][1]))  # add new index
-            tokens[counter].extend_array(data[i][1])
-            tokens[counter].add_doc(data[i][1])
-            tokens[counter].set_weight(data[i][1], 1)
-            tokens[counter].add_posting_to_doc(data[i][1], data[i][2])
-
-        print("index {} made".format(counter))
+            tokens.extend([Token])
+            tokens[counter] = Token(words[counter][0], words[counter][1])  # add new index
+            tokens[counter].add_doc(doc_id)
+            tokens[counter].set_weight(doc_id, 1)
+            tokens[counter].add_posting_to_doc(doc_id, posting)
 
 
 
